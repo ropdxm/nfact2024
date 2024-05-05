66ca7da992a511fbdc34cb35802237f661f94aae
Dulat <ropdxm@gmail.com>
2024-05-05 20:00:31 +0600

    login fb?

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useState, CSSProperties } from 'react';
 import './navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const [showIcon, setShowIcon] = useState<boolean>(true);
+    
   return (
 <header>
         <div className="logo">Spoty</div>
@@ -15,10 +17,10 @@ const Navbar = () => {
       </label>
 
       <nav className="navbar">
-        <a href="#" className="nav-item" style={{"--i": 0} as React.CSSProperties}>Home</a>
-        <a href="#" className="nav-item" style={{"--i": 1} as React.CSSProperties}>Plan Your Visit</a>
-        <a href="#" className="nav-item" style={{"--i": 2} as React.CSSProperties}>Magical Places</a>
-        <a href="#" className="nav-item" style={{"--i": 3} as React.CSSProperties}>Contact</a>
+        <Link to="/" className="nav-item" style={{"--i": 0} as CSSProperties}>Home</Link>
+        <Link to="/" className="nav-item" style={{"--i": 1} as CSSProperties}>Plan Your Visit</Link>
+        <Link to="/login" className="nav-item" style={{"--i": 2} as CSSProperties}>Login</Link>
+        <Link to="register" className="nav-item" style={{"--i": 3} as CSSProperties}>Sign Up</Link>
       </nav>
     </header>  )
 }
